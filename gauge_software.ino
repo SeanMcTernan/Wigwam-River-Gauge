84bed4070481b348422de9496bbe9f41f411c368
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2021-08-16 22:09:15 -0600

    Create template to get base read of river

--- gauge_software.ino
+++ gauge_software.ino
@@ -6,6 +6,8 @@
 SoftwareSerial sonarSerial(rxPin, txPin, true); //define serial port for recieving data, output from maxSonar is inverted requiring true to be set.
 
 boolean stringComplete = false;
+boolean stringComplete2 = false;
+boolean zeroCalculated = false;
 
 void setup()
 {
@@ -14,16 +16,21 @@ void setup()
   {
     ; // Wait to connect serial port. For native USB port only
   }
+  calculateZero();
+  while (!zeroCalculated)
+  {
+    ; // Wait to for base level to be calculated before initializing the loop
+  }
 }
 
 void loop()
 {
-  int range = serialValueRead();
-  if (stringComplete)
+  int range = serialValueRead2();
+  if (stringComplete2)
   {
-    stringComplete = false; //reset sringComplete ready for next reading
-    Serial.println(range);
-    delay(5000); //delay for debugging
+    stringComplete2 = false;                      //reset sringComplete ready for next reading
+    Serial.println((String) "Range is " + range); // Line for Debugging
+    delay(5000);                                  //delay for debugging
   }
 }
 
@@ -32,26 +39,21 @@ int serialValueRead()
   int result;
   char inData[4]; //char array to read data into
   int index = 0;
-
   sonarSerial.flush(); // Clear cache ready for next reading
   sonarSerial.begin(9600);
   while (stringComplete == false)
   {
-    //Serial.print("reading ");    //debug line
 
     if (sonarSerial.available())
     {
       char rByte = sonarSerial.read(); //read serial input for "R" to mark start of data
       if (rByte == 'R')
       {
-        //Serial.println("rByte set");
         while (index < 4) //read next three character for range from sensor
         {
           if (sonarSerial.available())
           {
             inData[index] = sonarSerial.read();
-            //Serial.println(inData[index]);               //Debug line
-
             index++; // Increment where to write next
           }
         }
@@ -62,6 +64,44 @@ int serialValueRead()
 
       index = 0;             // Reset index ready for next reading
       stringComplete = true; // Set completion of read to true
+      sonarSerial.end();
+      result = atoi(inData); // Changes string data into an integer for use
+    }
+  }
+
+  return result;
+}
+
+int serialValueRead2()
+{
+  int result;
+  char inData[4]; //char array to read data into
+  int index = 0;
+  sonarSerial.flush(); // Clear cache ready for next reading
+  sonarSerial.begin(9600);
+  while (stringComplete2 == false)
+  {
+
+    if (sonarSerial.available())
+    {
+      char rByte = sonarSerial.read(); //read serial input for "R" to mark start of data
+      if (rByte == 'R')
+      {
+        while (index < 4) //read next three character for range from sensor
+        {
+          if (sonarSerial.available())
+          {
+            inData[index] = sonarSerial.read();
+            index++; // Increment where to write next
+          }
+        }
+        inData[index] = 0x00; //add a padding byte at end for atoi() function
+      }
+
+      rByte = 0; //reset the rByte ready for next reading
+
+      index = 0;              // Reset index ready for next reading
+      stringComplete2 = true; // Set completion of read to true
       sonarSerial.end();
       result = atoi(inData); // Changes string data into an integer for use
     }
@@ -84,4 +124,39 @@ int calculateAverage()
   }
   result = firstReading[1];
   return result;
+}
+
+int calculateZero() //Calculate the base river level with 4 readings over the course of 1 minute
+{
+  int result;
+  result = 0;
+  int average;
+  int readings[4];
+  int readingIndex = 0;
+  while (readingIndex < 4)
+  {
+    readings[readingIndex] = serialValueRead();
+    if (stringComplete)
+    {
+      stringComplete = false;
+      Serial.println((String) "Reading at index " + readingIndex + " is " + readings[readingIndex]); // Line for Debugging
+      readingIndex++;
+      if (readingIndex != 4)
+      {
+        delay(3000);
+      }
+    }
+  }
+
+  unsigned int index;
+
+  for (index = 0; index < sizeof(readings) / sizeof(readings[0]); index++) // Add all values in the array
+  {
+    average += readings[index];
+  }
+  average = average / 4;
+
+  Serial.println((String) "The average level reading is " + average); // Line for Debugging
+  zeroCalculated = true;
+  return average;
 }
