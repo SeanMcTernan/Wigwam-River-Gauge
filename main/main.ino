b9ab1d82c8cbe8749e7206444dc1e1d1a0749ea3
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2021-09-24 12:32:52 -0600

    Code is working on a rolling 12 hr interval

--- main/main.ino
+++ main/main.ino
@@ -2,7 +2,7 @@
 
 #define MINUTE_VALUE 10
 #define HOUR_VALUE 60
-#define REPORT_PERIOD 8
+#define REPORT_PERIOD 12
 //Set the variables for the sensor reading
 static const int sensorReadPin = 7;
 int rangevalue[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -23,7 +23,6 @@ int levels[REPORT_PERIOD] = {0};
 int txMsgLen;
 int j, i = 0;
 bool beginOnce;
-
 char txBuffer[50] = {0};
 char rxBuffer[50] = {0};
 
@@ -81,17 +80,18 @@ void loop()
         }
     }
 
+    //An hour has passed reset the minute count and request the sensor to start reading again, also increment the hour reading
     if (minuteCount > HOUR_VALUE)
     {
         minuteCount = 0;
         hourCount++;
         measuring = true;
-        Serial.println((String) "New reading at hour " + hourCount);
     }
 
+    //8 Hours has passed, create the levels array and send it to the satelite. Reset the hours count to 0 to start the 8 hour cycle again.
     if (hourCount > REPORT_PERIOD - 1)
     {
-        lvl_message = "";
+        lvl_message = "[";
         for (j = 0; j < REPORT_PERIOD; j++)
         {
             lvl_message.concat(levels[j]);
@@ -101,61 +101,30 @@ void loop()
             }
             else
             {
-                lvl_message.concat("\0");
+                lvl_message.concat("]");
             }
         }
 
         txMsgLen = lvl_message.length() + 1;
         lvl_message.toCharArray(txBuffer, txMsgLen);
-        Serial.println((String) "txBuffer is " + txBuffer);
+        Serial.println(txBuffer);
         hourCount = 0;
     }
 }
 
-/*-----------Functions------------*/
-
-//Function to print the arrays.
-
-void printArray(int *a, int n)
-
-{
-
-    for (int i = 0; i < n; i++)
-
-    {
-
-        Serial.print(a[i], DEC);
-
-        Serial.print(' ');
-    }
-
-    Serial.println();
-}
-
 //Sorting function
-
-// sort function (Author: Bill Gentles, Nov. 12, 2010)
 
 void isort(int *a, int n)
 {
-
     //  *a is an array pointer function
-
     for (int i = 1; i < n; ++i)
-
     {
-
         int j = a[i];
-
         int k;
-
         for (k = i - 1; (k >= 0) && (j < a[k]); k--)
-
         {
-
             a[k + 1] = a[k];
         }
-
         a[k + 1] = j;
     }
 }
@@ -164,62 +133,42 @@ void isort(int *a, int n)
 
 int mode(int *x, int n)
 {
-
     int i = 0;
-
     int count = 0;
-
     int maxCount = 0;
-
     int mode = 0;
-
     int bimodal;
-
     int prevCount = 0;
 
     while (i < (n - 1))
     {
-
         prevCount = count;
-
         count = 0;
-
         while (x[i] == x[i + 1])
         {
-
             count++;
-
             i++;
         }
-
         if (count > prevCount & count > maxCount)
         {
-
             mode = x[i];
-
             maxCount = count;
-
             bimodal = 0;
         }
 
         if (count == 0)
         {
-
             i++;
         }
 
         if (count == maxCount)
         { //If the dataset has 2 or more modes.
-
             bimodal = 1;
         }
-
         if (mode == 0 || bimodal == 1)
         { //Return the median if there is no mode.
-
             mode = x[(n / 2)];
         }
-
         return mode;
     }
 }
