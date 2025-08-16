a7e69ea6ce50b2607109e37d419107e3b9adb6f8
Sean Mc Ternan <seanmcternan@gmail.com>
2025-08-16 09:12:51 -0600

    feat: add RTC serial configuration and update test message format

--- rockblock/sendMessage/sendMessage.ino
+++ rockblock/sendMessage/sendMessage.ino
@@ -131,10 +131,10 @@ void setup()
 
   // Send the message
   Serial.println(F("Trying to send the message.  This might take several minutes."));
-  err = modem.sendSBDText("[6720,22,6720,22,6720,22,6720,22,6720,22,6720,22,6720,22,6720,22,6720,22,6720,22,6720,22,6720,22]");
+  err = modem.sendSBDText("[125,125,125,125,125,125,125,125,125,125,125,125]");
   if (err != ISBD_SUCCESS)
   {
-    Serial.print(F("sendSBDText failed: error "));
+    Serial.print(F("sendSBDText failed: error "));  
     Serial.println(err);
     if (err == ISBD_SENDRECEIVE_TIMEOUT)
       Serial.println(F("Try again with a better view of the sky."));
